f2100d772e97a449cb4833db2f96d24ae6cf45f3
Axel-Dev3 <Auzerais.axel@protonmail.com>
2021-03-01 09:53:27 +0100

    modification de index

---
+++ inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="">
+    <title>Projet Git</title>
+  </head>
+  <body>
+    <header>
+
+    </header>
+
+    <main>
