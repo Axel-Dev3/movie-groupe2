e0075ff8d602d9e35d77f4e53164924dacdac3fc
Axel-Dev3 <Auzerais.axel@protonmail.com>
2021-03-01 10:22:07 +0100

    mise a jour et creation du dossier admin

---
+++ admin/inc/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="">
+    <title>Partie admin</title>
+  </head>
+  <body>
+    <header>
+
+    </header>
+
+    <main>
---
+++ admin/index.php
@@ -0,0 +1,5 @@
+<?php require('../inc/functions.php');
+
+require('inc/header.php'); ?>
+
+<?php require('inc/footer.php');
