1688c959743f6b506123aea443b0b802c4123213
Axel-Dev3 <Auzerais.axel@protonmail.com>
2021-03-01 12:25:02 +0100

    start index

--- inc/functions.php
+++ inc/functions.php
@@ -1,1 +1,11 @@
-<?php 
+<?php
+
+function debug (array $tab){
+  echo '<pre>';
+  echo print_r($tab);
+  echo '<pre>';
+}
+function imageMovie($movie)
+{
+    return '<img src="posters/' .$movie['id']. '.jpg" alt="' .$movie['title'].'">';
+}
--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php require('inc/functions.php');
+require('inc/pdo.php');
 
+
+$sql = "SELECT * FROM movies_full ORDER BY RAND()";
+$query = $pdo->prepare($sql);
+$query->execute();
+$movies = $query->fetchAll();
+
+// debug($movies);
 require('inc/header.php'); ?>
 
 <p><a href="admin/index.php">Admin</a></p>
+
+<?php foreach ($movies as $movie): ?>
+<div class="film">
+  <ul>
+    <li><?php echo imageMovie($movie); ?></li>
+    <li><a href="details.php?">Voir</a></li>
+  </ul>
+</div>
+
+<?php endforeach; ?>
 
 <?php require('inc/footer.php');
 
