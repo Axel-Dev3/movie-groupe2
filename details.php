2578f3a0a187480da2b8d04d914847b726ddaff1
Taaaz215 <maxime.carville@gmail.com>
2021-03-03 15:02:30 +0100

    rajout favoris recup id

--- details.php
+++ details.php
@@ -26,8 +26,8 @@ require('inc/header.php'); ?>
     <li><?= $movie['writers']; ?></li>
     <?php echo imageMovie($movie); ?>
     <li><a href="index.php">Retour</a></li>
-    <li><a href="favoris.php">favoris</a></li>
-
+    <li><a href="favoris.php?id=<?= $movie['id']; ?>">favoris</a></li>
+   
   </ul>
 </section>
 
--- favoris.php
+++ favoris.php
@@ -3,3 +3,27 @@ session_start();
 require('inc/functions.php');
 require('inc/pdo.php');
 require('inc/header.php');
+// verif utilisateur connecter 
+
+
+// afficher les favoris de l'utilisateur 
+if (!empty($_GET['id'])) 
+    {              // on verifie si l'id est present puis on le recupere 
+            $id = $_GET['id'];
+            echo $id; 
+
+        if (isLogged())
+            {
+             echo ('Film ajouter a vos favoris' );
+            } 
+        else
+        {
+            die('403');
+        }
+
+
+    }
+else {
+
+    die('404');
+}
